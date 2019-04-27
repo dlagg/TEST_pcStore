37c08870e30fb2aaf23b517d56e122ed59598010
dlag <idroj07@gmail.com>
2019-04-27 14:29:18 +0200

    Improvements on the interface and Invoice class

--- Invoice.cs
+++ Invoice.cs
@@ -4,17 +4,43 @@ namespace pcStore
 {
     public class Invoice
     {
-        private int mID,mTaxes,mTcost,mTcostDirty;
-        private const int tax = 15;
-        private ArrayList rowsProd = new ArrayList();
+        Customer xCustomer = new Customer();
+        Employee xEmployee = new Employee();
+        Product xProduct = new Product();
+        //Subordinated class
+        private class rowsProd
+        {
+            Product xProduct;
+            private string mProdName;
+            private int mProdPrice,mQuProd,mTotPrice;
+
+            public string ProdName{
+                get{ return mProdName; }
+                set{ mProdName = value; }
+            }
+            public int ProdPrice{
+                get{ return mProdPrice; }
+                set{ mProdPrice = value; }
+            }
+            public int QuProd{
+                get{ return mQuProd; }
+                set{ mQuProd = value; }
+            }
+            public int TotPrice{
+                get{ return mTotPrice; }
+                set{ mTotPrice = value; }
+            }
+            QuProd = 4; //test
+            TotPrice = QuProd * xProduct.Price;
+        }
+        //
+        private int mID,mTcost,mTcostDirty;
+        private const int mTax = 15;
+        private ArrayList allRows = new ArrayList();
 
         public int ID{
             get{ return mID; }
             set{ mID = value; }
-        }
-        public int Taxes{
-            get{ return mTaxes; }
-            set{ mTaxes = value; }
         }
         public int Tcost{
             get{ return mTcost; }
@@ -27,8 +53,12 @@ namespace pcStore
 
         public string showData()
         {
+            
             string data="";
 
+            data+="--------INVOICE ID: "+ID+"--------"+"\n"
+            +"Customer: "+xCustomer.Name+" / Employee: "+xEmployee.Name+". "+xEmployee.ID+"\n"
+            +
 
 
             return data;
--- Program.cs
+++ Program.cs
@@ -7,11 +7,51 @@ namespace pcStore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("### PCSTORE MAIN MENU ### "+"\n"+"Input de number for selected option:"+"\n"+
-            "1 - Search."+"\n"+"2 - Add."+"\n"+"3 - Delete."+"\n"+"\n"
-            );
-            Console.Write(/*mostrar datos de facturas,productos,clientes,empleados*/"");
+            bool closeApp = false;
+            while (closeApp == false)
+            {
+                Console.WriteLine("### PCSTORE MAIN MENU ### "+"\n"+
+                "AQUI mostrar datos de facturas,productos,clientes,empleados"
+                );
+                Console.Write("Input de number for selected option:"+"\n"+
+                "1 - Search."+"\n"+"2 - Add."+"\n"+"3 - Delete."+"\n"+
+                "4 - Exit Program.");
+                int selection = int.Parse(Console.ReadLine());
 
+                switch (selection)
+                {
+                    case 1: //Search
+                    break;
+                    
+                    case 2: //Add
+                    {
+                        Console.Write("Select:"+"\n"+
+                        "1 - Customer."+"\n"+"2 - Product."+"\n"+"3 - Employee."+"\n"+
+                        "4 - Sale."+"\n"+"5 - Back."+"\n"+"6 - Exit Program.");
+                        int addSel = int.Parse(Console.ReadLine());
+
+                        switch (addSel)
+                        {
+                            
+                            case 1: //ADD CUSTOMERS
+                            {
+                                
+                                break;
+                            }
+                        }
+                        break;
+                    }
+
+                    case 3: //Delete
+                    break;
+
+                    case 4: //Exit
+                    {
+                        closeApp = true;
+                        break;
+                    }
+                }
+            }
         }
     }
 }
